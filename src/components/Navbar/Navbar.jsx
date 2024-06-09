889dfeb118329138e87c2d67b4d543312394a1cc
masaapotat <masaakavulanya@gmail.com>
2024-06-09 11:04:48 +0300

    initialproject setup and setting up the file structure

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        masaa sasa
+    </div>
+  )
+}
+
+export default Navbar
