1debcbd8fc5471705acf00a7e3e42bd04238d969
masaapotat <masaakavulanya@gmail.com>
2024-06-09 19:17:34 +0300

    adding social media icons on the footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './Footer.css'
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import XIcon from '@mui/icons-material/X';
 
 const Footer = () => {
   return (
-    <div className='footer'></div>
+    <div className='footer'>
+      <div className="footer-icons">
+        <YouTubeIcon/>
+        <InstagramIcon/>
+        <XIcon/>
+        <FacebookIcon/>
+      </div>
+    </div>
   )
 }
 
