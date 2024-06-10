a5027758c3cbb1091c0a10eb32bf1ff6610f66fd
masaapotat <masaakavulanya@gmail.com>
2024-06-10 07:07:33 +0300

    creating the login page and making a route path to it

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import Home from './pages/Home/Home'
+import React from "react";
+import Home from "./pages/Home/Home";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login/Login";
 const App = () => {
   return (
     <div>
-      <Home/>
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/*this wrong because both routes have the same path ('/'). This means that only one of the components will be rendered when you navigate to '/', and it doesn't allow you to navigate to the Login component.    
+        <Route path='/' element={<Login/>}/> */}
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './Login.css'
+// import mylogo from '../../assets/mylogo.jpg'
 
+import logo from '../../assets/mylogo.jpg'
 const Login = () => {
   return (
-    <div className='login'></div>
+    <div className='login'>
+      <img src={logo} alt="" className='login-logo'/>
+      {/* <img src={logo} alt=''/> */}
+      <div className="login-form">
+        <h1>Sign In</h1>
+        <form action=""></form>
+      </div>
+    </div>
   )
 }
 
