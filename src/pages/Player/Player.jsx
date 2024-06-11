a8f6eaf733c02fe7854fcca7295ea34c4126e150
masaapotat <masaakavulanya@gmail.com>
2024-06-11 06:43:32 +0300

    imprting arrow forwards and backwards arrow icons fro material ui

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import './Player.css'
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 const Player = () => {
   return (
-    <div className='player'></div>
+    <div className='player'>
+      <ArrowBackIosIcon/>
+      <ArrowForwardIosIcon/>
+
+    </div>
   )
 }
 
