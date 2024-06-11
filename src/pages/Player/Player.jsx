c2610207bf77cfc046978d6bc972cb25c57fd19a
masaapotat <masaakavulanya@gmail.com>
2024-06-11 06:43:32 +0300

    creating a html structure of the player component

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
-import './Player.css'
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import React from "react";
+import "./Player.css";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 const Player = () => {
   return (
-    <div className='player'>
-      <ArrowBackIosIcon/>
-      <ArrowForwardIosIcon/>
-
+    <div className="player">
+      <ArrowBackIosIcon />
+      <ArrowForwardIosIcon />
+      <iframe
+        width="90%"
+        height="90%"
+        src="https://www.youtube.com/embed/IFkHrwtHRY8"
+        title="trailer"
+        frameBorder="0"
+        allowFullScreen
+      ></iframe>
+      <div className="player-info">
+        <p>Published Date</p>
+        <p>Name</p>
+        <p>Type</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Player
+export default Player;
