5a4054400b9741e196d3b48daa601e68d2a62722
masaapotat <masaakavulanya@gmail.com>
2024-06-13 06:34:39 +0300

    code for getting the youtube video by id

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -4,6 +4,20 @@ import ArrowCircleLeftOutlinedIcon from '@mui/icons-material/ArrowCircleLeftOutl
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 const Player = () => {
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1Mjc0MzNmODVjMDQ5NTlmZGYxNWUyZTU4OGY5MTIyZCIsInN1YiI6IjY2MmI1M2UwM2Q3NDU0MDExZGQyMjg4NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lofHSDEDVsm_GWFQYs8wbChCX1ZDGecHFYrE084Y4Ys'
+    }
+  };
+  
+  fetch('https://api.themoviedb.org/3/movie/653346/videos?language=en-US', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+  
   return (
     <div className="player">
       <ArrowCircleLeftOutlinedIcon className="back-icon" />
