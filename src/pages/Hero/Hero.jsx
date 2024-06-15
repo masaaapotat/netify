b68979187254fbb6bbac3cc4401657ad6ee62c40
masaapotat <masaakavulanya@gmail.com>
2024-06-15 21:00:20 +0300

    adding isLoading

---
+++ src/pages/Hero/Hero.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import "./Hero.css";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import InfoIcon from "@mui/icons-material/Info";
+
+const Hero = () => {
+  const [movie, setMovie] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1Mjc0MzNmODVjMDQ5NTlmZGYxNWUyZTU4OGY5MTIyZCIsInN1YiI6IjY2MmI1M2UwM2Q3NDU0MDExZGQyMjg4NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.lofHSDEDVsm_GWFQYs8wbChCX1ZDGecHFYrE084Y4Ys",
+      },
+    };
+
+    fetch(
+      "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
+      options
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const randomMovie =
+          data.results[Math.floor(Math.random() * data.results.length)];
+        setMovie(randomMovie);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) return <div className="loading">Loading...</div>;
+
+  if (!movie) return <div className="error">Error loading data</div>;
+
+  return (
+    <div className="hero">
+      <img
+        src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+        alt={movie.title}
+        className="banner-img"
+      />
+      <div className="hero-caption">
+        <h1>{movie.title}</h1>
+        <p>{movie.overview}</p>
+        <div className="hero-btn">
+          <button className="btn">
+            <PlayArrowIcon className="hero-btn-icon" />
+            Play
+          </button>
+          <button className="btn dark-btn">
+            <InfoIcon className="hero-btn-icon" />
+            More Info
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Home.css";
+import Hero from "../Hero/Hero";
 import Navbar from "../../components/Navbar/Navbar";
-import hero_banner from "../../assets/hero_banner.jpg";
-import hero_title from "../../assets/hero_title.png";
-import PlayArrowIcon from "@mui/icons-material/PlayArrow";
-import InfoIcon from "@mui/icons-material/Info";
+// import hero_banner from "../../assets/hero_banner.jpg";
+// import hero_title from "../../assets/hero_title.png";
+// import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+// import InfoIcon from "@mui/icons-material/Info";
 import TitleCards from "../../components/TitleCards/TitleCards";
 import Footer from "../../components/Footer/Footer";
 
@@ -12,8 +13,9 @@ const Home = () => {
   return (
     <div className="home">
       <Navbar />
+      <Hero />
       {/* creating the Hero section */}
-      <div className="hero">
+      {/* <div className="hero">
         <img src={hero_banner} alt="" className="banner-img" />
         <div className="hero-caption">
           <img src={hero_title} alt="" className="captiom-img" />
@@ -25,18 +27,18 @@ const Home = () => {
           <div className="hero-btn">
             <button className="btn">
               {" "}
-              <PlayArrowIcon />
+              <PlayArrowIcon className=".hero-btn" />
               Play
             </button>
             <button className="btn dark-btn">
               {" "}
-              <InfoIcon />
+              <InfoIcon className=".hero-btn" />
               More Info
             </button>
           </div>
           <TitleCards />
         </div>
-      </div>
+      </div> */}
       <div className="more-cards">
         <TitleCards title={"Blockbuster Movies"} category="top_rated" />
         <TitleCards title={"Only on Netflix"} category="popular" />
