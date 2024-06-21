63b73e629d91ede6feca07c55adbade7f7bbf3bd
masaapotat <masaakavulanya@gmail.com>
2024-06-21 11:41:13 +0300

    works

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./pages/Home/Home";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Player from "./pages/Player/Player";
-import FavoriteMovies from "./pages/Search/FavoriteMovies";
+
 const App = () => {
   return (
     <div>
--- src/pages/Search/Genredropdown.jsx
+++ src/pages/Search/Genredropdown.jsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Genredropdown.css";
 
 const GenreDropdown = ({ genres, selectedGenre, handleGenreChange }) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const handleGenreClick = (genreId) => {
+    handleGenreChange({ target: { value: genreId } });
+    setIsDropdownOpen(false);
+  };
+
   return (
-    <select value={selectedGenre} onChange={handleGenreChange} className="genre-dropdown">
-      <option value="">Select Genre</option>
-      {genres.map((genre) => (
-        <option key={genre.id} value={genre.id}>
-          {genre.name}
-        </option>
-      ))}
-    </select>
+    <div className="genre-dropdown-container">
+      <div className="genre-dropdown" onClick={toggleDropdown}>
+        {selectedGenre ? genres.find((genre) => genre.id === selectedGenre)?.name : "Select Genre"}
+      </div>
+      {isDropdownOpen && (
+        <ul className="genre-dropdown-options">
+          {genres.map((genre) => (
+            <li key={genre.id} onClick={() => handleGenreClick(genre.id)}>
+              {genre.name}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   );
 };
 
